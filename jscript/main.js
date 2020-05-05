91d1e25292f94f339e0da00104a33ad6919ea155
Jsingh25 <singhjaskaranpreet@gmail.com>
2020-05-05 15:20:54 -0500

    Grid Added

--- jscript/main.js
+++ jscript/main.js
@@ -1,1 +1,42 @@
-alert('Hello World');
+var BoxLength = document.getElementById("BoxLen")
+var BoxWidth = document.getElementById("BoxWid")
+var BagLength = document.getElementById("BagLen")
+var BagWidth = document.getElementById("BagWid")
+var Gusset = document.getElementById("Gusset")
+
+var cols = 10;
+var rows = 10;
+
+var colors = [];
+
+function make2Darray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+function setup() {
+  createCanvas(300, 300);
+  colors = make2Darray(cols, rows);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      colors[i][j] = (255);
+    }
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      var x = i * 30;
+      var y = j * 30;
+      fill(colors[i][j]);
+      stroke(0);
+      rect(x, y, 30, 30);
+    }
+  }
+}
